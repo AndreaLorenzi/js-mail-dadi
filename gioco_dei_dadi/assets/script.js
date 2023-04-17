bcdafde06bd7a29cb888f53b0344d6d454b9fa59
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-04-17 17:54:17 +0200

    add random number of player and computer

---
+++ gioco_dei_dadi/assets/script.js
@@ -0,0 +1,13 @@
+// creare numero casuale per utente e computer
+// verificare i numeri creati
+// dichiarare il vincitore 
+// se utente maggiore del computer allora l'utente vince
+// se computer maggiore dell'utente allora l'utente perde
+// se i numeri sono uguali allora pareggio
+
+const player = Math.round(Math.random () * 5)+ 1;
+console.log(player)
+const computer = Math.round(Math.random () * 5)+ 1;
+console.log(computer)
+
+
